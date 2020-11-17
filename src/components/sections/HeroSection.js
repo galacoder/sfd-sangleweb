389a9a411679a36841b4e87f37ef53ee286fc907
Sang Le <sangle@Sangs-MacBook-Pro.local>
2020-11-17 15:30:12 +0700

    set initial styled component for HeroSection

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,15 +1,40 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 function HeroSection() {
   return (
-    <div>
-      <img src="/images/logos/sangletech-logo.png" alt="logo" />
-      <h1>VƯƠN TẦM BẢN THÂN</h1>
-      <p>BƯỚC ĐI TỰ TIN TRÊN HÀNH TRÌNH 4.0</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </div>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>VƯƠN TẦM BẢN THÂN</Title>
+          <Description>BƯỚC ĐI TỰ TIN TRÊN HÀNH TRÌNH 4.0</Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 
 export default HeroSection
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #343563 0%, #4926ad 100%);
+  margin: 0 auto;
+  max-width: 1234px;
+  padding: 200px 30px;
+`
+const ContentWrapper = styled.div``
+const TextWrapper = styled.div`
+  width: 360px;
+  display: grid;
+  gap: 30px;
+`
+
+const Title = styled.h1`
+  font-size: 60px;
+  color: white;
+`
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`
